2ccc358bac1605f7b3ba767cdc8ec9b975116519
Kojji <fernandokojiyama@gmail.com>
2023-03-23 10:41:11 +1000

    add: list of notifications

--- src/store/modules/misc.js
+++ src/store/modules/misc.js
@@ -1,35 +1,30 @@
   const state = {
-    notificationToggle: false,
-    notificationInformation: {
-      duration: 5,
-      message: "",
-      type: "alert" //success, fail, alert
-    }
+    notificationList: []
   }
   
   const mutations = {
-    setNotificationToggle(state, value) {
-      state.notificationToggle = value;
+    setNotificationList(state, value) {
+      state.notificationList = value;
     },
-    setNotificationInformation(state, value) {
-      state.notificationInformation = value;
-    }
   }
   
   const actions = {
     activateNotification(state, information) {
-      state.commit("setNotificationToggle", true)
-      state.commit("setNotificationInformation", information)
+      let notifications = JSON.parse(JSON.stringify(state.getters['getNotificationList']))
+      notifications.push(information)
+      state.commit("setNotificationList", notifications)
+    },
+    removeNotification(state) {
+      let notifications = JSON.parse(JSON.stringify(state.getters['getNotificationList']))
+      notifications.splice(0, 1)
+      state.commit("setNotificationList", notifications)
     }
   }
   
   const getters = {
-    getNotificationToggle(state) {
-      return state.notificationToggle;
+    getNotificationList(state) {
+      return state.notificationList;
     },
-    getNotificationInformation(state) {
-      return state.notificationInformation;
-    }
   }
   
   export default {

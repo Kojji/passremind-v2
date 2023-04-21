52193944e5a9ab61ae5f1a38e5afd9e650d95c52
Kojji <fernandokojiyama@gmail.com>
2023-04-21 21:59:06 +1000

    add:new implementation of plugin, tailwind setup

---
+++ plugin/src/popup/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-pink-500">
+    <span class="text-lg text-green-700">New Text</span>
+    <hello-world />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: { HelloWorld },
+};
+</script>
+
+<style>
+html {
+  width: 400px;
+  height: 400px;
+}
+</style>
--- plugin/vue.config.js
+++ plugin/vue.config.js
@@ -1,4 +1,26 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  chainWebpack: (config) => {
+    config.optimization.delete("splitChunks");
+  },
+
+  transpileDependencies: true,
+
+  pages: {
+    popup: {
+      template: "public/browser-extension.html",
+      entry: "./src/popup/main.js",
+      title: "Popup",
+    },
+  },
+
+  pluginOptions: {
+    browserExtension: {
+      componentOptions: {
+        background: {
+          entry: "src/background.js",
+        },
+      },
+    },
+  },
+});
